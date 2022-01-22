aaf1ffd5fc462692faff804caf5340b07f69023e
Aswin K <aswinkarat@gmail.com>
2022-01-22 22:55:11 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#inlcude <stdio.h>
+
+const int MAX_SIZE = 24;
+
+void playMinesweeper() {}
+
+void chooseDificulty() {}
+
+void assignMoves() {}
+
+void countAdjacentMines() {}
+
+void isValid() {}
+
+void isMine() {}
+
+void displayBoard() {}
+
+void placeMines() {}
+
+void openAdjacentTiles() {}
+
+void main() {
+    char realBoard[25][25], myBoard[25][25];
+}
