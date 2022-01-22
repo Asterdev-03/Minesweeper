5468b3bc24cfcaceccf4219bbb755ae8ce07423b
Aswin K <aswinkarat@gmail.com>
2022-01-22 23:07:46 +0530

    init

--- main.cpp
+++ main.cpp
@@ -1,25 +1,48 @@
+// Minesweeper Game
+
 #inlcude <stdio.h>
 
 const int MAX_SIZE = 24;
 
-void playMinesweeper() {}
+// Continues the game 
+void playMinesweeper() {
+    
+}
 
-void chooseDificulty() {}
+// To choose the difficulty of the game
+void chooseDificulty(*size) {
+    int choice;
+}
 
+// To assign the user's move
 void assignMoves() {}
 
-void countAdjacentMines() {}
+// To count the adjacent mines of the tile
+int countAdjacentMines() {
+    int count;
+    return count;
+}
 
+// To check validity of user's move
 void isValid() {}
 
+// To check if the tile is a mine
 void isMine() {}
 
+// To display the Board to the user
 void displayBoard() {}
 
+// To randomly place the mines
 void placeMines() {}
 
+// To open the adjacent tiles
 void openAdjacentTiles() {}
 
-void main() {
+int main() {
     char realBoard[25][25], myBoard[25][25];
+    int boardSize;
+    chooseDifficulty(&boardSize);
+    placeMines();
+    playMinesweeper();
+    return 0;
 }
